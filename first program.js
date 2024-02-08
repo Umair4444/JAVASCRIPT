df637225a0c0198862c9b9c570a083c3d44b1254
Umair4444 <theumairkhan44@gmail.com>
2024-02-08 17:05:19 +0500

    MY FIRST PROGRAM

--- first program.js
+++ first program.js
@@ -43,3 +43,89 @@
 // console.log(animal_kingdom);
 // console.log(animal_kingdom.birds);
 // console.log(animal_kingdom.reptile[1]);
+
+// console.log("set time out example");
+
+// // login user with set time out
+// const loginuser = (username, email) => {
+//   setTimeout(() => {
+//     console.log("you can logged in", username); // print login user
+//     return email; // return nothing
+//   }, 5000);
+// };
+
+// loginuser("umair", "@gmail.com");
+// // console.log(loginuser("umair", "@gmail.com")); // return undefine
+
+// // // // login user with set time out and callback example 1 with function built in parameter
+// const loginuser = (username, email,callback) => {
+//   setTimeout(() => {
+//     // console.log("you can logged in", username); // print login user
+//     return callback(username,email)  // return nothing
+//   }, 5000);
+// };
+
+// loginuser("umair", "@gmail.com",(username,email)=>{console.log("callback function is executed","username is", username,
+// "and email is",email);});
+// // console.log(loginuser("umair", "@gmail.com")); // return undefine
+
+// // login user with set time out and callback  example 2 with function built seperately
+// const loginuser = (username, email, callback) => {
+//     setTimeout(() => {
+//       console.log("You can log in", username); // print login user
+//       callback(); // invoke the callback function
+//     }, 5000);
+//   };
+
+//   // Define the callback function
+//   const callbackFunction = () => {
+//     console.log("Callback executed");
+//   };
+
+//   // Call loginuser with the correct callback
+//   loginuser("umair", "@gmail.com", callbackFunction);
+
+// function add(a, b) {
+//   return (c = a + b);
+// }
+
+// function square(c) {
+//   return (d = c * c);
+// }
+
+// square(2) // passing square as parameter in adding and then adding
+// console.log(add(d,2));
+
+// // console.log(square(add(2, 2))); // getting square after adding in one line
+
+// add(5, 1); // passing parameter in square func
+
+// console.log(add(c)); //getting square after adding
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
